da0a1b7ff3ddf72fe213b84a0afaa007b6e622d6
尘心 <smilewupeng@gmail.com>
2022-05-07 15:31:34 +0800

    feat: 子应用 js 的加载和解析

--- micro/loader/index.js
+++ micro/loader/index.js
@@ -5,7 +5,7 @@ export const loader = async (app) => {
     // 子应用内容显示容器 - container
 
     //  子应用入口文件 - entry
-    const html = await parseHtml(entry)
+    const [dom, script] = await parseHtml(entry)
 
     let ct = document.querySelector(container)
 
@@ -14,7 +14,9 @@ export const loader = async (app) => {
     }
 
     //  容器内容重写
-    ct.innerHTML = html
+    ct.innerHTML = dom
+
+    console.log(dom, script, '-loader')
 
     return app
 }
@@ -24,15 +26,68 @@ export const parseHtml = async (entry) => {
     //  通过请求获取html -> fetch
     let html = await fetchReSource(entry)
 
+    //  全部 script 内容集合
+    let allScripts = []
+
     const div = document.createElement('div')
     div.innerHTML = html
 
     //  处理标签, link, script(src, js)
-    // const [dom, scripUrl, script] = await parseJS()
+    const [dom, scripUrl, script] = await parseJS(div, entry)
 
-    return html;
+    //  获取 js 的内容
+    let fetchScripts = await Promise.all(scripUrl.map(async item => await fetchReSource(item)));
+
+    allScripts = script.concat(fetchScripts)
+
+    return [dom, allScripts];
 }
 
-export const parseJS = async () => {
-    // retrun [dom, scripUrl, script]
+export const parseJS = async (root, entry) => {
+    const dom = root.outerHTML;    //  包含本身在内的 所有html 内容
+    const scripUrl = [];
+    const script = [];
+
+    //  深度解析
+    function deepParse(element) {
+        const children = element.children;
+        const parent = element.parent;
+
+        //  处理 script 中的内容
+        if (element.nodeName.toLowerCase() === 'script') {
+            let src = element.getAttribute('src')
+            if (!src) {
+                script.push(element.outerHTML)
+            } else {
+                if (src.startsWith('http')) {
+                    scripUrl.push(src);
+                } else {
+                    scripUrl.push(`http:${entry}/${src}`);
+                }
+            }
+        }
+
+        if (parent) {
+            parent.replaceChild(document.createComment(`此 js 文件已经被微前端替换`), element)
+        }
+
+        //  处理 link 中也会有 js 内容
+        if (element.nodeName.toLowerCase() === 'link') {
+            let href = element.getAttribute('href')
+            if (href.endsWith('.js')) {
+                if (href.startsWith('http')) {
+                    scripUrl.push(href)
+                } else {
+                    scripUrl.push(`http:${entry}/${href}`)
+                }
+            }
+        }
+
+        for (let i = 0; i < children.length; i++) {
+            deepParse(children[i])
+        }
+    }
+
+    deepParse(root)
+    return [dom, scripUrl, script]
 }
